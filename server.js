9c3f2d4ecf649e865e8131a0edaf4fd1ab9e55a0
Matheus Bolson <mathbolson@hotmail.com>
2020-09-23 14:09:11 -0700

    working on the javascript. Created the prompts and answers. Now will work on each function

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var inquirer = require("inquirer");
 var consoleTable = require("console.table");
 var mysql = require("mysql");
+const { type } = require("os");
+const { listenerCount } = require("process");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,3 +16,77 @@ connection.connect(function (err) {
   if (err) throw err;
   runSearch();
 });
+
+function runSearch() {
+  inquirer
+  .prompt({
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: [
+      "view all employees",
+      "view all departments",
+      "view all managers",
+      "add employee",
+      "add department",
+      "add role",
+      "remove employee",
+      "remove role",
+      "remove manager",
+      "exit"
+    ]
+
+  })
+  .then(function (answers) {
+    console.log(answers.action);
+    switch (answers.action) {
+      case "view all employees":
+        employeeView();
+        break;
+
+        case "view all departments":
+          departmentView();
+          break;
+
+      case "view all managers":
+        managerView();
+      break;
+
+      case "add employee":
+        employeeAdd();
+        break;
+
+      case "add department":
+        departmentAddd();
+        break;
+
+      case "add role":
+        roleAdd();
+        break;
+
+      case "remove employee":
+        employeeRemove();
+        break
+
+      case "remove role":
+        roleRemove();
+        break;
+      
+      case "remove manager":
+        managerRemove();
+        break;
+
+      case "exit":
+        connection.end();
+        break;
+      }
+
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+}
