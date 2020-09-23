69330303a0315515a020767c49ce5c1b52bd888a
Matheus Bolson <mathbolson@hotmail.com>
2020-09-23 11:55:55 -0700

    added mysql tables and seeds

---
+++ server.js
@@ -0,0 +1,16 @@
+var inquirer = require("inquirer");
+var consoleTable = require("console.table");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "employee_DB"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  runSearch();
+});
